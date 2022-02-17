cc8464d17edcb5539c8e0d48bb33a6f471d68204
Ajebli_Ahmed <78688533+Ahmed-ajb@users.noreply.github.com>
2022-02-18 00:10:08 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -1,24 +1,21 @@
 <?php
-include "dbconnect.php"
-$objectPdo->prepare('INSERT INTO `utilisateurs' VALUES (:Nom_user,:Prenom_user,:Sexe,:Date_naissance,:Pays,:Num_tel,:Email,:password,:ville)');
-    $pdostat->bindValue(':Nom_user,$_POST['nom'],PDO::PARAM_STR);
-$pdostat->bindValue(':Prenom_user,$_POST['prenom'],PDO::PARAM_STR);
-    $pdostat->bindValue(':Sexe,$_POST['Sexe'],PDO::PARAM_STR);
-$pdostat->bindValue(':Date_naissance,$_POST['date'],PDO::PARAM_STR);
-    $pdostat->bindValue(':Pays,$_POST['pays'],PDO::PARAM_STR);
-$pdostat->bindValue(':Num_tel,$_POST['phone'],PDO::PARAM_STR);
-    $pdostat->bindValue(':Email,$_POST['email'],PDO::PARAM_STR);
-$pdostat->bindValue(':password,$_POST['Mot de passe'],PDO::PARAM_STR);
-    $pdostat->bindValue(':ville,$_POST['ville'],PDO::PARAM_STR);
-$inserer=$pdostat->execute();
-if($inserer){
-    $message='utilisateur ajouté';
-}else{
-    $message='insertion echouée';
-
+    session_start();
+   $servername = "localhost";
+$username = "root";
+$password = "";
+if(ISSET($_POST['inscrire'])){
+        if($_POST['nom'] != "" || $_POST['prenom'] != "" || $_POST['email'] != ""|| $_POST['Mot de passe'] != "" || $_POST['Date de naissance'] != ""|| $_POST['sexe'] != "" || $_POST['pays'] != ""|| $_POST['ville'] != "" || $_POST['phone'] != ""){
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=towatch", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                $sql = "INSERT INTO `utilisateurs` VALUES ('1', '$nom', '$prenom', '$email', '$pass','$sexe','$date_naissance','$pays','$ville','$phone')";
+                $conn->exec($sql);
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
 }
-$conn = null;
-  
-   
-?>
+ $conn = null;}}
+    
 ?>
