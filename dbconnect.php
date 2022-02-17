977fdaaba22c80f005578d68625bf2a946f637e5
Ajebli_Ahmed <78688533+Ahmed-ajb@users.noreply.github.com>
2022-02-17 01:55:25 +0100

    conn

--- dbconnect.php
+++ dbconnect.php
@@ -1,4 +1,12 @@
- 
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cours PHP / MySQL</title>
+        <meta charset="utf-8">
+        <link rel="stylesheet" href="cours.css">
+    </head>
+    <body>
+        <h1>Bases de données MySQL</h1>  
         <?php
             $servername = 'localhost';
             $username = 'root';
@@ -13,3 +21,5 @@
             }
             echo 'Connexion réussie';
         ?>
+    </body>
+</html>
--- inscription.php
+++ inscription.php
@@ -9,15 +9,8 @@ if (isset($_POST['submit'])) {
     $date=$_POST['Date_naissance'];
     $pays=$_POST['pays'];
     $ville=$_POST['ville'];
-
-    $date=$_POST['Date de naissance'];
-    $pays=$_POST['pays'];
-     $ville=$_POST['ville'];
-    $tel=$_POST['Num_tel'];
-    $query="INSERT INTO utilisateurs(Nom_user,Prenom_user,Email,password,Sexe,Date_naissance,Pays,ville,Num_tel)VALUES('$nom','$prénom','$email','$password','$sexe','$date','$pays','$ville,$tel');";
-
     $tel=$_POST['phone'];
-    $query="INSERT INTO utilisateurs(Nom_user,Prenom_user,Email,password,Sexe,Date_naissance,Pays,Num_tel)VALUES('$nom','$prénom','$email','$password','$sexe','$date','$pays','$tel');";
+    $query="INSERT INTO utilisateurs(Nom_user,Prenom_user,Email,password,Sexe,Date_naissance,Pays,ville,Num_tel)VALUES('$nom','$prénom','$email','$password','$sexe','$date','$pays','$ville','$tel');";
     
     $con = mysql_connect("localhost","root" ,"");//for wamp 3rd feild is balnk
 if (!$con)
