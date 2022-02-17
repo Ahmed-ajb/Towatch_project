62d6968e0dc09ec815e6727bc85decc35cd6e67a
zainabjl <zainab.joual@usmba.ac.ma>
2022-02-17 10:24:39 +0100

    changement du submit

--- inscription.php
+++ inscription.php
@@ -1,17 +1,16 @@
 <?php
 include "dbconnect.php"
-if (isset($_POST['submit'])) {
+if (isset($_POST['Inscrire'])) {
     $nom=$_POST['nom'];
     $prénom=$_POST['prenom'];
+    $sexe=$_POST['Sexe'];
+    $date=$_POST['Date de naissance'];
+    $pays=$_POST['pays'];
+    $tel=$_POST['phone'];
     $email=$_POST['email'];
     $password=$_POST['Mot de passe'];
-    $sexe=$_POST['Sexe'];
-    $date=$_POST['Date_naissance'];
-    $pays=$_POST['pays'];
-    $ville=$_POST['ville'];
-    $tel=$_POST['phone'];
-    $query="INSERT INTO utilisateurs(Nom_user,Prenom_user,Email,password,Sexe,Date_naissance,Pays,ville,Num_tel)VALUES('$nom','$prénom','$email','$password','$sexe','$date','$pays','$ville','$tel');";
-    
+     $ville=$_POST['ville'];
+    $query="INSERT INTO utilisateurs(Id_user,Nom_user,Prenom_user,Sexe,Date_naissance,Pays,Num_tel,Email,password,ville)VALUES('1','$nom','$prénom','$sexe','$date','$pays','$tel','$email','$password','$ville');";
     $con = mysql_connect("localhost","root" ,"");//for wamp 3rd feild is balnk
 if (!$con)
 {
