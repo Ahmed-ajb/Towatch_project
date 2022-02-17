22119a8a08443584cbfd5d778778c9f03cdf74cf
zainabjl <zainab.joual@usmba.ac.ma>
2022-02-17 01:35:48 +0100

    Update inscription.php

--- inscription.php
+++ inscription.php
@@ -9,7 +9,7 @@ if (isset($_POST['submit'])) {
     $date=$_POST['Date de naissance'];
     $pays=$_POST['pays'];
      $ville=$_POST['ville'];
-    $tel=$_POST['Num_tel'];
+    $tel=$_POST['phone'];
     $query="INSERT INTO utilisateurs(Nom_user,Prenom_user,Email,password,Sexe,Date_naissance,Pays,Num_tel)VALUES('$nom','$prénom','$email','$password','$sexe','$date','$pays','$tel');";
     
     $con = mysql_connect("localhost","root" ,"");//for wamp 3rd feild is balnk
