5c467ee58d5c405c213c98c4231d5bcce08e2001
zainabjl <zainab.joual@usmba.ac.ma>
2022-03-11 09:25:07 +0100

    Update index.php

--- index.php
+++ index.php
@@ -15,7 +15,7 @@ session_start();
       <meta name="viewport" content="width=device-width, initial-scale=1">
       <meta name="viewport" content="initial-scale=1, maximum-scale=1">
       <!-- site metas -->
-      <title>ToWatch</title>
+      <title>ToWatch1</title>
       <meta name="keywords" content="">
       <meta name="description" content="">
       <meta name="author" content="">
