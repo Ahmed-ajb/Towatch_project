505f54ee949d2e86c73fd7fdb0a6827554fd7315
Ajebli_Ahmed <78688533+Ahmed-ajb@users.noreply.github.com>
2022-03-31 15:20:27 +0000

    INDEX

--- index.php
+++ index.php
@@ -190,10 +190,10 @@ include("dbconnect.php");
                           <div class="page_title">
                              <!--search barre -->
       <div class="d-flex justify-content-lefter h-100">
-        <div class="searchbar">
+        <form action="search.php" method="GET"><div class="searchbar">
           <input class="search_input" type="text" name="" placeholder="Search...">
           <a href="#" class="search_icon"><i class="fa fa-search"></i></a>
-        </div>
+        </div></form>
     </div>
                              <!--  -->
 
